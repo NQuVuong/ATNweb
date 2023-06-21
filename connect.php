a913b8e3c15a45d07903ee703299ddf645d7e054
Nguyen Quoc Vuong <121931993+NQuVuong@users.noreply.github.com>
2023-06-21 21:03:48 +0700

    Update connect.php

--- connect.php
+++ connect.php
@@ -7,10 +7,10 @@ class connect{
     public $dbName;
 
     public function __construct(){
-        $this->server ="frwahxxknm9kwy6c.cbetxkdyhwsb.us-east-1.rds.amazonaws.com";
-        $this->user = "i54w038ndaay5n8z";
-        $this->password = "bnrnw1ov0awatnl0";
-        $this->dbName = "i8sdvey43k92ljgl";
+        $this->server ="vkh7buea61avxg07.cbetxkdyhwsb.us-east-1.rds.amazonaws.com";
+        $this->user = "rya5zh8a5cmnk8rk";
+        $this->password = "htlp2qgr4ld9ylpd";
+        $this->dbName = "rqd3pifcb25mlgmk";
     }
 
     function connectToMySQL():mysqli{
